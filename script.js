1bc94555e6a665534ac9dfb4f36ac562d0114036
Martynenko <4lexm4rtin@gmail.com>
2020-05-02 02:50:28 +0300

    fixed_adding_used_letters/fewer_points

--- script.js
+++ script.js
@@ -105,66 +105,149 @@ var words = [
   "нетрадиционность",
   "социология" 
 ];
+var words1 = [
+  "авиакомпания",
+  "агентство",
+  "аэропорт",
+  "велосипед",
+  "гостиница",
+  "автобус",
+  "достопримечательность",
+  "иностранец",
+  "международный",
+  "каникулы",
+  "направление",
+  "побережье",
+  "путешествие",
+  "путевка",
+  "экзотический",
+  "чемодан",
+  "сувенир",
+  "миграция",
+  "прибывать",
+  "пассажир" 
+];
+var words2 = [
+  "исследование",
+  "бюрократия",
+  "индустриализация",
+  "марксизм",
+  "конформизм",
+  "общество",
+  "государство",
+  "благосостояние",
+  "контроль",
+  "информация",
+  "идентичность",
+  "структура",
+  "концепция",
+  "демократичность",
+  "нетрадиционность",
+  "социология" 
+];
 
-var word = words[Math.floor(Math.random() * words.length)];
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-  answerArray[i] = "_";
-} 
+function pickWord(arrayWords) {
+  return arrayWords[Math.floor(Math.random() * arrayWords.length)];
+}
+
+function setupAnswerArray(word) {
+  var tempArray = [];
+  for (var i = 0; i < word.length; i++) {
+    tempArray[i] = "_";
+  } 
+  return tempArray;
+}
+
+function getGuess() {
+  return prompt(answerArray.join(" ") + 
+  " ---У вас осталось попыток: <" + 
+  remainingPoints + 
+  "> Угадайте букву или нажмите Отмена для выхода из игры." + 
+  " Вы уже вводили такие буквы: " + stringUsedLetters(usedLetters));
+}
+
+function stringUsedLetters(usedLetters) {
+  var tempString = "-";
+  for (var usedLettersLength = 0; usedLettersLength < usedLetters.length; usedLettersLength++) {
+    tempString += (usedLetters[usedLettersLength] + "-");
+  }
+  return tempString;
+}
+
+function remainingPoint() {
+  if (word.length > 14) {
+    return 12;
+  } else if (word.length > 9) {
+    return 8;
+  } else {
+    return 5;
+  }
+}
+
+var word = pickWord(words);
+var answerArray = setupAnswerArray(word);
+
 
 var remainingLetters = word.length;
-var remainingPoints = word.length;
+var remainingPoints = jfcrremainingPoint()
 var usedLetters = [];
 
 while (remainingPoints > 0 && remainingLetters > 0) {
-  var guess = prompt(answerArray.join(" ") + 
-    " ---У вас осталось попыток: <" + 
-    remainingPoints + 
-    "> Угадайте букву или нажмите Отмена для выхода из игры." + 
-    " Вы уже вводили такие буквы: " + usedLetters)
+  var guess = getGuess();
   if (guess === null) {
     break;
   } else if ( guess.length < 1) {
     alert("Пожалуйста, введите хотя бы одну букву.");
   } else {
     for (var y = 0; y < guess.length; y++) {
-      var errorGuess = word.length;
-      if (usedLetters.length !== 0) {
-        var e = 0;
-        for (var q = 0; q < (usedLetters.length - e); q++) {
-          e = 0;
-          if (guess[y] === usedLetters[q]) {                
-            alert("Вы уже вводили букву: " + guess[y]);     //..........
-          } else {                                          //..........
+      if (guess[y] === " ") {
+        alert("Нужно вводить буквы, а не пробелы.");
+      } else {
+        var v = 0;
+        var errorGuess = word.length;
+        if (usedLetters.length !== 0) {
+          var e = 0;
+          for (var q = 0; q < usedLetters.length; q++) {
+            if (guess[y] === usedLetters[q]) {                
+              alert("Вы уже вводили букву: " + guess[y]); 
+              q += usedLetters.length;
+              v = 1;
+            } else {                                          //..........
+              e++;
+            }
+          }
+          if (e === usedLetters.length) {
             usedLetters += guess[y];                        //..........
-            e = usedLetters.length;
+          }
+        } 
+        if (v === 0) {
+          var x1 = 0;
+          var x2 = 0;
+          for (var j = 0; j < (word.length - x1); j++) {
+            if (answerArray[j] !== "_" && answerArray[j] === guess[y]) {
+              alert("Эта буква уже отгадана.")
+              x1 = word.length;
+            } else if (word[j] === guess[y]) {
+              if (x2 != 0) {
+                alert("Ура, такая буква есть аж в " + (x2 + 1) +" местах!");
+              } else {
+                alert("Ура, такая буква есть!");
+              }
+              answerArray[j] = guess[y];
+              remainingLetters--;
+              x2++;
+            } else {
+              errorGuess--;
+              if (usedLetters.length === 0) {
+                usedLetters += guess[y];
+              }
+            }
+          }
+          if (errorGuess === 0) {
+            remainingPoints--;
+            alert("Сорян, но буква: " + guess[y] + ", не найдена." + " Минус одна попытка!");
           }
         }
-      } 
-      var x = 0;
-      for (var j = 0; j < (word.length - x); j++) {
-        if (answerArray[j] !== "_" && answerArray[j] === guess[y]) {
-          alert("Эта буква уже отгадана.")
-          x = word.length;
-        } else if (word[j] === guess[y]) {
-          if (x != 0) {
-            alert("Ура, такая буква есть аж в " + (x + 1) +" местах!");
-          } else {
-            alert("Ура, такая буква есть!");
-          }
-          answerArray[j] = guess[y];
-          remainingLetters--;
-          x++;
-        } else {
-          errorGuess--;
-          if (usedLetters.length === 0) {
-            usedLetters += guess[y];
-          }
-        }
-      }
-      if (errorGuess === 0) {
-        remainingPoints--;
-        alert("Сорян, но буква: " + guess[y] + ", не найдена." + " Минус одна попытка!");
       }
     }
   }
